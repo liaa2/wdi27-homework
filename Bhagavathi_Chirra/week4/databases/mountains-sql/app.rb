994ba1ea23030fbbb38493f60cc009f24add436b
Bhagavathi Chirra <bhagavathi.chirra@gmail.com>
2018-05-13 09:47:43 +1000

    mountain update

--- Bhagavathi_Chirra/week4/databases/mountains-sql/app.rb
+++ Bhagavathi_Chirra/week4/databases/mountains-sql/app.rb
@@ -24,3 +24,16 @@ get "/mountains/:id/edit" do
   @mountain = Mountain.find params[:id]
   erb :mountains_edit
 end
+
+post "/mountains/:id" do
+  mountain = Mountain.find params[:id]
+  mountain.name = params[:name]
+  mountain.height = params[:height]
+  mountain.range = params[:range]
+  mountain.parent_mountain = params[:parent_mountain]
+  mountain.first_ascent = params[:first_ascent]
+  mountain.country = params[:country]
+  mountain.image_url = params[:image_url]
+  mountain.save
+  redirect "/mountains/#{params[:id]}"
+end
