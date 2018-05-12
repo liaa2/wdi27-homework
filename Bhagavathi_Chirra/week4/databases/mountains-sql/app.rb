be7a215fbdb845692c195519e7dcc6f7283589fd
Bhagavathi Chirra <bhagavathi.chirra@gmail.com>
2018-05-13 08:53:46 +1000

    mountains index,show

--- Bhagavathi_Chirra/week4/databases/mountains-sql/app.rb
+++ Bhagavathi_Chirra/week4/databases/mountains-sql/app.rb
@@ -1,3 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "database.db"
+)
+
+class Mountain < ActiveRecord::Base
+end
+
+get "/mountains" do
+  @mountains = Mountain.all
+  erb :mountains_index
+end
+
+get "/mountains/:id" do
+  @mountain = Mountain.find params[:id]
+  erb :mountain_show
+end
+
+get "/mountains/:id/edit" do
+  @mountain = Mountain.find params[:id]
+  erb :mountains_edit
+end
