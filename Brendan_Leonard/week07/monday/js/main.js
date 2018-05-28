f305eb80febf0e3d548d9c0182e8b84c100b4319
Brendan Leonard <brendanleonard99@gmail.com>
2018-05-29 09:17:55 +1000

    Refactored with single Ajax call function

--- Brendan_Leonard/week07/monday/js/main.js
+++ Brendan_Leonard/week07/monday/js/main.js
@@ -23,65 +23,28 @@ const makeAjaxCall = function( url, methodType, callbackFunc ){
 
 const getImgBaseUrl = function( apiRespJson ) {
   baseUrl = apiRespJson.images.base_url;
-  console.log('Base URL = ' + baseUrl);
-  
 }
 
-const getImageURLs = function() {
-  const configQuery = 'https://api.themoviedb.org/3/configuration?api_key=373ba99aa87c1ac696ca9fc120752b25';
-  const xhr = new XMLHttpRequest();
-  xhr.open( 'GET', configQuery );
-  xhr.send();
+const showMovies = function ( apiRespJson ) {
+  const totalResults = apiRespJson.total_results;
+  const moviesReturned = apiRespJson.results;
+  const totalPages = apiRespJson.total_pages;
 
-  xhr.onreadystatechange = function() {
-    if ( xhr.readyState !== 4 ){
-      return;
-    }
-    const queryResponse = JSON.parse( xhr.response );
-    console.log(queryResponse);
-    baseURL = queryResponse.images.base_url;
-  };
-}
+  // Display number returned field
+  $("#search-message").append("<h5>" + totalResults + " movies returned by your search </h5>");
 
-const getMovies = function( searchString ) {
-  // Get first page
-  const searchPrefix = 'https://api.themoviedb.org/3/search/movie?api_key=373ba99aa87c1ac696ca9fc120752b25';
-  // const searchToRun = searchPrefix + '&query=' + searchString + '&page=' + pageNumber;
+  // Display individual image posters
+  moviesReturned.forEach(movie => {
+    let poster = movie.poster_path;
+    let img = '';
 
-  let i = 0;
-  let totalPages = 3;
-
-  while ( i < totalPages ){
-    i++;
-    const searchToRun = searchPrefix + '&query=' + searchString + '&page=' + i;
-
-    const xhr = new XMLHttpRequest();
-    xhr.open( 'GET', searchToRun );
-    xhr.send();
-
-    xhr.onreadystatechange = function() {
-      if ( xhr.readyState !== 4 ){
-        return;
-      }
-      const queryResponse = JSON.parse( xhr.response );
-      const totalResults = queryResponse.total_results;
-      const moviesReturned = queryResponse.results;
-      const totalPages = queryResponse.total_pages;
-      $("#search-message").append("<h5>" + totalResults + " movies returned by your search </h5>");
-
-      moviesReturned.forEach(movie => {
-        let poster = movie.poster_path;
-        let img;
-
-        if( poster ){
-          img = $("<div class='poster-image'><img src='" + baseUrl + "/w342" + movie.poster_path + "'></img><div class='centered-title'>" + movie.title + "</div></div>");
-        } else {
-          img = $("<div class='poster-image'><img src='https://www.fillmurray.com/342/513'></img><div class='centered-title'>" + movie.title + "</div>");
-        }        
-        $(".row").append( img );
-      });
-    } // end while
-  }
+    if( poster ){
+      img = $("<div class='poster-image'><img src='" + baseUrl + "/w342" + movie.poster_path + "'></img><div class='centered-title'>" + movie.title + "</div></div>");
+    } else {
+      img = $("<div class='poster-image'><img src='https://www.fillmurray.com/342/513'></img><div class='centered-title'>" + movie.title + "</div>");
+    }        
+    $(".row").append( img );
+  }) // end forEach
 }
 
 $(function(){
@@ -89,18 +52,20 @@ $(function(){
   let apiKey = '?api_key=373ba99aa87c1ac696ca9fc120752b25';
 
   // Get image Url
-  let urlRequest = urlPrefix + 'configuration' + apiKey;
-  console.log('>>>' + urlRequest + '<<<');
-  
+  let urlRequest = urlPrefix + 'configuration' + apiKey;  
   makeAjaxCall( urlRequest, 'GET', getImgBaseUrl );
-
-  // getImageURLs();
 
   $('#search-form').on('submit', function(){
     $("#search-message").empty();
     $(".row").empty();
 
-    getMovies($('#search-field').val());
+    // Get movies
+    let searchString = $('#search-field').val().trim();
+    if( searchString.length > 0 ){
+      let urlRequest = urlPrefix + 'search/movie' + apiKey + '&query=' + searchString;
+      makeAjaxCall( urlRequest, 'GET', showMovies );
+    }
+    // getMovies($('#search-field').val());
     return false;
   })
 });
