03452f34dd43f6166c8034923eb9f1a857fcb06b
Anna Tran <anhkaehne@gmail.com>
2018-06-25 21:09:34 +1000

    week 11 monday hw

--- Hoang_Anh_Tran/week11/regular-expressions/bonus_round.rb
+++ Hoang_Anh_Tran/week11/regular-expressions/bonus_round.rb
@@ -1,7 +1,5 @@
 require 'pry'
 
-
-
 # Extra credit exercise: write a program that prints out any input line that mentions both wilma and fred.
 print "Bonus: match both wilma and fred: \n"
 ARGF.each do |line|
